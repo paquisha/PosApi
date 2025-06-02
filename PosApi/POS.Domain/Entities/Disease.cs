cd4b2c503b7c8ec7af85122d30e22d4fab25b67d
Andres Grijalva <paquishaxd@gmail.com>
2025-06-01 21:51:40 -0500

    🦖 aun no termino la configuracion 2

--- PosApi/POS.Domain/Entities/Disease.cs
+++ PosApi/POS.Domain/Entities/Disease.cs
@@ -8,35 +8,20 @@ using System.Threading.Tasks;
 
 namespace POS.Domain.Entities
 {
-    [Table("Diseases")]
     public class Disease : BaseEntity
     {
-        [StringLength(10)]
         public string? Code { get; set; }
-
-        [Required]
-        [Column(TypeName = "nvarchar(MAX)")]
         public string Name { get; set; }
-
-        [Column(TypeName = "nvarchar(MAX)")]
         public string? Description { get; set; }
-
-        [Column(TypeName = "nvarchar(MAX)")]
         public string? Actions { get; set; }
 
         [Required]
         [ForeignKey("DiseaseType")]
         public int DiseaseTypeId { get; set; }
         public virtual DiseaseType? DiseaseType { get; set; }
-        [InverseProperty("Disease")]
         public virtual ICollection<Diagnostic> Diagnostics { get; set; } = new List<Diagnostic>();
 
         [InverseProperty("Diseases")]
         public virtual ICollection<MedicalRecord> MedicalRecords { get; set; } = new List<MedicalRecord>();
-
-        public Disease()
-        {
-            Name = string.Empty;
-        }
     }
 }
--- PosApi/POS.Infraestructure/Persistences/Contexts/Configuration/DiseaseConfiguration.cs
+++ PosApi/POS.Infraestructure/Persistences/Contexts/Configuration/DiseaseConfiguration.cs
@@ -13,11 +13,16 @@ namespace POS.Infraestructure.Persistences.Contexts.Configuration
     {
         public void Configure(EntityTypeBuilder<Disease> builder)
         {
+            builder.ToTable("Diseases");
             builder.HasKey(keyExpression: e => e.Id);
             builder.Property(e => e.Id).HasColumnName("DiseaseId");
 
-            builder.Property(propertyExpression: e => e.Code).IsUnicode(unicode: false);
-            builder.Property(propertyExpression: e => e.Name).IsUnicode(unicode: false);
+            builder.Property(propertyExpression: e => e.Code).HasMaxLength(10).IsUnicode(unicode: false);;
+            builder.Property(propertyExpression: e => e.Name)
+                .IsRequired()
+                .HasColumnType("varchar(MAX)");
+            builder.Property(propertyExpression: e => e.Description).HasColumnType("varchar(MAX)");
+            builder.Property(propertyExpression: e => e.Actions).HasColumnType("varchar(MAX)");
         }
     }
 }
