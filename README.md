2258df9226c42f302fcd9b7434a75e46df3daf36
paquisha <paquishaxd@gmail.com>
2025-07-20 23:25:36 -0500

    se agregan cambios estables para levantar app

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # PosApi
 plantilla base
+## crear la DB
+ingresa en el directorio final
+
+instar dependencias
+```shell
+amarumed-back/yarn install
+amarumed-front/yarn install
+```
+
+## crear la DB
+
+Para inicializar una DB de prueba ejecutar las siguientes lineas de comando.
+
+```shell
+createdb -U postgres amarumed
+```
+
+Si se crea por primera vez la base de datos.
+```shell
+createdb -U postgres amarumed
+yarn build 
+yarn migrate
+```
+Si desea borrar y crearla nuevamente.
+```shell
+dropdb -U postgres amarumed 
+createdb -U postgres amarumed 
+yarn build 
+yarn migrate
+```
+
+## ejecutar los scripts de /sql
+en el gestor de base de datos
+```shell
+sql/ROLES.sql
+sql/MEDICALEXAMS.sql
+sql/etc..
+```
+
+## levantar la app
+```shell
+amarumed-back/yarn serve
+amarumed-front/yarn serve
+```
---
+++ final/amarumed-front/src/views/medrec/pending/Prescription.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-form ref="form" v-model="isValidForm" lazy-validation>
+    <v-card color="grey lighten-4" class="particle">
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-textarea
+              v-model="element.reason"
+              background-color="white"
+              label="Descripción"
+              :rules="rules.reason"
+              outlined
+            />
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" @click="update()" text>
+          Siguiente <v-icon right>fa-angle-right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import service from '@/services/MedicalRecordService'
+import Component from 'vue-class-component'
+import { Watch } from 'vue-property-decorator'
+import { Prop } from 'vue-property-decorator'
+import { createMedicalRecord } from '@/models'
+import validate from '@/utils/validations'
+import { MedicalRecord } from '@/models'
+import alert from '@/utils/alert'
+
+@Component({})
+export default class PrescriptionComponent extends Vue {
+    @Prop() medRec!: MedicalRecord
+
+
+}
+</script>
